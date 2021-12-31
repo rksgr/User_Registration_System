3b3b6a265580d268c604e73461c7ac6399ccd95b
rksgr <r_kjha@yahoo.com>
2021-12-31 12:18:39 +0530

    Refactored: User email validation

--- UserCredentialValidation.java
+++ UserCredentialValidation.java
@@ -27,10 +27,32 @@ public class UserCredentialValidation {
         return last_Name_correct;
     }
 
+    /*
+    Use case 3: Valid Email (3 mandatory parts and 1 optional part)
+     */
+
+    public boolean userEmailIdVerification(String usr_email_id) {
+        Pattern pattern = Pattern.compile("[a-z0-9]{3,}([.+_-][a-zA-Z0-9]{1,}|[a-zA-Z0-9]*)[@]" +
+                "[a-z0-9]{1,}[.]([a-z]{2,}[.][a-z]{2,}|[a-z]{2,})");
+        Matcher matcher = pattern.matcher(usr_email_id);
+        Boolean emailCorrect = matcher.matches();
+        if (emailCorrect){
+            System.out.println("Correct Email.");
+        }else {
+            System.out.println("Incorrect Email.");
+        }
+        return emailCorrect;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to BridgeLabz!");
         System.out.println("Today we shall verify credentials of a user.");
         UserCredentialValidation uc = new UserCredentialValidation();
-        uc.userLastNameVerification("Tom");
+        uc.userEmailIdVerification("abc+100@gmail.com");
+        uc.userEmailIdVerification("abc-111@abc.net");
+        uc.userEmailIdVerification("abc.100@abc.com.au");
+        uc.userEmailIdVerification("abc.@gmail.com");
+        uc.userEmailIdVerification("abc@abc@gmail.com");
+        uc.userEmailIdVerification("abc..2002@gmail.com");
     }
 }
