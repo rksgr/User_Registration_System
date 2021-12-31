35f50923ebd7eda2019fa95e717cf2fe8d1fd0db
rksgr <r_kjha@yahoo.com>
2021-12-31 10:26:40 +0530

    Added: User first name verification

--- UserCredentialValidation.java
+++ UserCredentialValidation.java
@@ -1,9 +1,24 @@
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class UserCredentialValidation {
+    /*
+    Use case 1: Valid first name (first name starts with capital letters and has
+                minimum three characters)
+     */
+
+    public boolean userFirstNameVerification(String usr_first_name) {
+        Pattern pattern = Pattern.compile("[A-Z][a-z]{2,}");
+        Matcher matcher = pattern.matcher(usr_first_name);
+        Boolean first_Name_correct = matcher.matches();
+        System.out.println(first_Name_correct);
+        return first_Name_correct;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to BridgeLabz!");
         System.out.println("Today we shall verify credentials of a user.");
+        UserCredentialValidation uc = new UserCredentialValidation();
+        uc.userFirstNameVerification("ert@yahoo.com");
     }
 }
