098e65d022ff1f16dacb871019fcd6d7dc489a2a
rksgr <r_kjha@yahoo.com>
2021-12-31 10:20:58 +0530

    Added: Welcome Message

---
+++ UserCredentialValidation.java
@@ -0,0 +1,9 @@
+
+
+public class UserCredentialValidation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BridgeLabz!");
+        System.out.println("Today we shall verify credentials of a user.");
+    }
+}
